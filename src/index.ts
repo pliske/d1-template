726d56cddfdb5b1d649b5cb42d6c4b8ca7bfa1d3
Peter Liske <peter@liske.org>
2025-07-12 13:19:06 -0700

    Update index.ts
    query picklists

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { renderHtml } from "./renderHtml";
 
 export default {
   async fetch(request, env) {
-    const stmt = env.DB.prepare("SELECT * from utm_campaign_v");
+    const stmt = env.DB.prepare("SELECT * from picklists");
     const { results } = await stmt.all();
 
     return new Response(renderHtml(JSON.stringify(results, null, 2)), {
